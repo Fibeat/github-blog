b71373f4d906eb78647b2f60d367e4d56d1c61c1
Prometheus <3483921926@qq.com>
2026-02-06 12:38:06 +0800

    str.py

---
+++ docs/Python/hsp-file/08-数据容器/05_str.py
@@ -0,0 +1,9 @@
+# 字符串的使用
+# 参考文档：https://docs.python.org/zh-cn/3.14/library/stdtypes.html#text-sequence-type-str
+
+# 1.字符串的定义：
+# 1、在Python中处理文本数据是使用str对象,也称为 字符串。字符串是由Unicode码位构成的不可变序列
+# - 老韩解读
+# 1)Unicode码是一种字符编码
+# 2)参考:https://zhidao.baidu.com/question/1376934773881722859.html
+# 3)参考:ord()返回单个字符对应的Unicode编码值
